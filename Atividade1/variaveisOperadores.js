6196aecb085cbad99506069448de9405804ddecb
Rafael Rodrigues Martins <104734420+RafaelRM1@users.noreply.github.com>
2024-03-14 23:34:08 -0300

    atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let numA1 = 10;
+let numB1 = 5;
+let numA2 = 20;
+let numB2 = 3;
+
+let resultadoSoma = numA1 + numB1;
+let resultadoSubtracao = numA1 - numB1;
+let resultadoMultiplicacao = numA2 * numB2;
+let resultadoDivisao = numA2 / numB2;
+
+console.log("Resultado da soma entre ",numA1," e ",numB1,":",resultadoSoma);
+console.log("Resultado da subtração entre ",numA1," e ",numB1,":",resultadoSubtracao);
+console.log("Resultado da multiplicação entre ",numA2," e ",numB2,":",resultadoMultiplicacao);
+console.log("Resultado da divisão entre ",numA2," e ",numB2,":",resultadoDivisao);
